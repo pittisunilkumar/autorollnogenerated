a30afad569edd32a6b30a8ab23e1568f67bc9a01
Pitti Sunil Kumar <pittisunilkumar4@gmail.com>
2023-09-13 02:34:19 +0530

    first commit

---
+++ sch_settings.sql
@@ -0,0 +1,217 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 12, 2023 at 06:14 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sunill`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sch_settings`
+--
+
+CREATE TABLE `sch_settings` (
+  `id` int(11) NOT NULL,
+  `base_url` varchar(500) DEFAULT NULL,
+  `folder_path` text DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `biometric` int(11) DEFAULT 0,
+  `biometric_device` text DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `phone` varchar(50) DEFAULT NULL,
+  `address` text DEFAULT NULL,
+  `lang_id` int(11) DEFAULT NULL,
+  `languages` varchar(500) NOT NULL,
+  `dise_code` varchar(50) DEFAULT NULL,
+  `date_format` varchar(50) NOT NULL,
+  `time_format` varchar(255) NOT NULL,
+  `currency` varchar(50) NOT NULL,
+  `currency_symbol` varchar(50) NOT NULL,
+  `is_rtl` varchar(10) DEFAULT 'disabled',
+  `is_duplicate_fees_invoice` varchar(100) DEFAULT '0',
+  `collect_back_date_fees` int(11) NOT NULL,
+  `single_page_print` int(1) DEFAULT 0,
+  `timezone` varchar(30) DEFAULT 'UTC',
+  `session_id` int(11) DEFAULT NULL,
+  `cron_secret_key` varchar(100) NOT NULL,
+  `currency_place` varchar(50) NOT NULL DEFAULT 'before_number',
+  `currency_format` varchar(20) DEFAULT NULL,
+  `class_teacher` varchar(100) NOT NULL,
+  `start_month` varchar(40) NOT NULL,
+  `attendence_type` int(10) NOT NULL DEFAULT 0,
+  `low_attendance_limit` float(10,2) NOT NULL,
+  `image` varchar(100) DEFAULT NULL,
+  `admin_logo` varchar(255) NOT NULL,
+  `admin_small_logo` varchar(255) NOT NULL,
+  `admin_login_page_background` varchar(255) NOT NULL,
+  `user_login_page_background` varchar(255) NOT NULL,
+  `theme` varchar(200) NOT NULL DEFAULT 'default.jpg',
+  `fee_due_days` int(3) DEFAULT 0,
+
+
+  `adm_auto_insert` int(1) NOT NULL DEFAULT 1,
+  `adm_prefix` varchar(50) NOT NULL DEFAULT 'ssadm19/20',
+  `adm_start_from` varchar(11) NOT NULL,
+  `adm_no_digit` int(10) NOT NULL DEFAULT 6,
+  `adm_update_status` int(11) NOT NULL DEFAULT 0,
+
+
+  `sroll_auto_insert` int(1) NOT NULL DEFAULT 1,
+  `sroll_prefix` varchar(50) NOT NULL DEFAULT '',
+  `sroll_start_from` varchar(11) NOT NULL,
+  `sroll_no_digit` int(10) NOT NULL DEFAULT 6,
+  `sroll_update_status` int(11) NOT NULL DEFAULT 0,
+
+
+  `staffid_auto_insert` int(11) NOT NULL DEFAULT 1,
+  `staffid_prefix` varchar(100) NOT NULL DEFAULT 'staffss/19/20',
+  `staffid_start_from` varchar(50) NOT NULL,
+  `staffid_no_digit` int(11) NOT NULL DEFAULT 6,
+  `staffid_update_status` int(11) NOT NULL DEFAULT 0,
+
+
+  `is_active` varchar(255) DEFAULT 'no',
+  `online_admission` int(1) DEFAULT 0,
+  `online_admission_payment` varchar(50) NOT NULL,
+  `online_admission_amount` float NOT NULL,
+  `online_admission_instruction` text NOT NULL,
+  `online_admission_conditions` text NOT NULL,
+  `online_admission_application_form` varchar(255) DEFAULT NULL,
+  `exam_result` int(11) NOT NULL,
+  `is_blood_group` int(10) NOT NULL DEFAULT 1,
+  `is_student_house` int(10) NOT NULL DEFAULT 1,
+  `roll_no` int(11) NOT NULL DEFAULT 1,
+  `category` int(11) NOT NULL,
+  `religion` int(11) NOT NULL DEFAULT 1,
+  `cast` int(11) NOT NULL DEFAULT 1,
+  `mobile_no` int(11) NOT NULL DEFAULT 1,
+  `student_email` int(11) NOT NULL DEFAULT 1,
+  `admission_date` int(11) NOT NULL DEFAULT 1,
+  `lastname` int(11) NOT NULL,
+  `middlename` int(11) NOT NULL DEFAULT 1,
+  `student_photo` int(11) NOT NULL DEFAULT 1,
+  `student_height` int(11) NOT NULL DEFAULT 1,
+  `student_weight` int(11) NOT NULL DEFAULT 1,
+  `measurement_date` int(11) NOT NULL DEFAULT 1,
+  `father_name` int(11) NOT NULL DEFAULT 1,
+  `father_phone` int(11) NOT NULL DEFAULT 1,
+  `father_occupation` int(11) NOT NULL DEFAULT 1,
+  `father_pic` int(11) NOT NULL DEFAULT 1,
+  `mother_name` int(11) NOT NULL DEFAULT 1,
+  `mother_phone` int(11) NOT NULL DEFAULT 1,
+  `mother_occupation` int(11) NOT NULL DEFAULT 1,
+  `mother_pic` int(11) NOT NULL DEFAULT 1,
+  `guardian_name` int(1) NOT NULL,
+  `guardian_relation` int(11) NOT NULL DEFAULT 1,
+  `guardian_phone` int(1) NOT NULL,
+  `guardian_email` int(11) NOT NULL DEFAULT 1,
+  `guardian_pic` int(11) NOT NULL DEFAULT 1,
+  `guardian_occupation` int(1) NOT NULL,
+  `guardian_address` int(11) NOT NULL DEFAULT 1,
+  `current_address` int(11) NOT NULL DEFAULT 1,
+  `permanent_address` int(11) NOT NULL DEFAULT 1,
+  `route_list` int(11) NOT NULL DEFAULT 1,
+  `hostel_id` int(11) NOT NULL DEFAULT 1,
+  `bank_account_no` int(11) NOT NULL DEFAULT 1,
+  `ifsc_code` int(1) NOT NULL,
+  `bank_name` int(1) NOT NULL,
+  `national_identification_no` int(11) NOT NULL DEFAULT 1,
+  `local_identification_no` int(11) NOT NULL DEFAULT 1,
+  `rte` int(11) NOT NULL DEFAULT 1,
+  `previous_school_details` int(11) NOT NULL DEFAULT 1,
+  `student_note` int(11) NOT NULL DEFAULT 1,
+  `upload_documents` int(11) NOT NULL DEFAULT 1,
+  `student_barcode` int(11) NOT NULL DEFAULT 1,
+  `staff_designation` int(11) NOT NULL DEFAULT 1,
+  `staff_department` int(11) NOT NULL DEFAULT 1,
+  `staff_last_name` int(11) NOT NULL DEFAULT 1,
+  `staff_father_name` int(11) NOT NULL DEFAULT 1,
+  `staff_mother_name` int(11) NOT NULL DEFAULT 1,
+  `staff_date_of_joining` int(11) NOT NULL DEFAULT 1,
+  `staff_phone` int(11) NOT NULL DEFAULT 1,
+  `staff_emergency_contact` int(11) NOT NULL DEFAULT 1,
+  `staff_marital_status` int(11) NOT NULL DEFAULT 1,
+  `staff_photo` int(11) NOT NULL DEFAULT 1,
+  `staff_current_address` int(11) NOT NULL DEFAULT 1,
+  `staff_permanent_address` int(11) NOT NULL DEFAULT 1,
+  `staff_qualification` int(11) NOT NULL DEFAULT 1,
+  `staff_work_experience` int(11) NOT NULL DEFAULT 1,
+  `staff_note` int(11) NOT NULL DEFAULT 1,
+  `staff_epf_no` int(11) NOT NULL DEFAULT 1,
+  `staff_basic_salary` int(11) NOT NULL DEFAULT 1,
+  `staff_contract_type` int(11) NOT NULL DEFAULT 1,
+  `staff_work_shift` int(11) NOT NULL DEFAULT 1,
+  `staff_work_location` int(11) NOT NULL DEFAULT 1,
+  `staff_leaves` int(11) NOT NULL DEFAULT 1,
+  `staff_account_details` int(11) NOT NULL DEFAULT 1,
+  `staff_social_media` int(11) NOT NULL DEFAULT 1,
+  `staff_upload_documents` int(11) NOT NULL DEFAULT 1,
+  `staff_barcode` int(11) NOT NULL DEFAULT 1,
+  `staff_notification_email` varchar(50) NOT NULL,
+  `mobile_api_url` tinytext NOT NULL,
+  `app_primary_color_code` varchar(20) DEFAULT NULL,
+  `app_secondary_color_code` varchar(20) DEFAULT NULL,
+  `admin_mobile_api_url` tinytext NOT NULL,
+  `admin_app_primary_color_code` varchar(20) NOT NULL,
+  `admin_app_secondary_color_code` varchar(20) NOT NULL,
+  `app_logo` varchar(250) DEFAULT NULL,
+  `student_profile_edit` int(1) NOT NULL DEFAULT 0,
+  `start_week` varchar(10) NOT NULL,
+  `my_question` int(1) NOT NULL,
+  `superadmin_restriction` varchar(20) NOT NULL,
+  `student_timeline` varchar(20) NOT NULL,
+  `calendar_event_reminder` int(2) DEFAULT NULL,
+  `event_reminder` varchar(20) NOT NULL,
+  `student_login` varchar(100) DEFAULT NULL,
+  `parent_login` varchar(100) DEFAULT NULL,
+  `student_panel_login` int(1) NOT NULL DEFAULT 1,
+  `parent_panel_login` int(1) NOT NULL DEFAULT 1,
+  `is_student_feature_lock` int(1) NOT NULL DEFAULT 0,
+  `maintenance_mode` int(1) NOT NULL DEFAULT 0,
+  `lock_grace_period` int(10) NOT NULL DEFAULT 0,
+  `is_offline_fee_payment` int(1) NOT NULL DEFAULT 0,
+  `offline_bank_payment_instruction` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_at` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `sch_settings`
+--
+
+INSERT INTO `sch_settings` (`id`, `base_url`, `folder_path`, `name`, `biometric`, `biometric_device`, `email`, `phone`, `address`, `lang_id`, `languages`, `dise_code`, `date_format`, `time_format`, `currency`, `currency_symbol`, `is_rtl`, `is_duplicate_fees_invoice`, `collect_back_date_fees`, `single_page_print`, `timezone`, `session_id`, `cron_secret_key`, `currency_place`, `currency_format`, `class_teacher`, `start_month`, `attendence_type`, `low_attendance_limit`, `image`, `admin_logo`, `admin_small_logo`, `admin_login_page_background`, `user_login_page_background`, `theme`, `fee_due_days`, `adm_auto_insert`, `adm_prefix`, `adm_start_from`, `adm_no_digit`, `adm_update_status`, `staffid_auto_insert`, `staffid_prefix`, `staffid_start_from`, `staffid_no_digit`, `staffid_update_status`, `is_active`, `online_admission`, `online_admission_payment`, `online_admission_amount`, `online_admission_instruction`, `online_admission_conditions`, `online_admission_application_form`, `exam_result`, `is_blood_group`, `is_student_house`, `roll_no`, `category`, `religion`, `cast`, `mobile_no`, `student_email`, `admission_date`, `lastname`, `middlename`, `student_photo`, `student_height`, `student_weight`, `measurement_date`, `father_name`, `father_phone`, `father_occupation`, `father_pic`, `mother_name`, `mother_phone`, `mother_occupation`, `mother_pic`, `guardian_name`, `guardian_relation`, `guardian_phone`, `guardian_email`, `guardian_pic`, `guardian_occupation`, `guardian_address`, `current_address`, `permanent_address`, `route_list`, `hostel_id`, `bank_account_no`, `ifsc_code`, `bank_name`, `national_identification_no`, `local_identification_no`, `rte`, `previous_school_details`, `student_note`, `upload_documents`, `student_barcode`, `staff_designation`, `staff_department`, `staff_last_name`, `staff_father_name`, `staff_mother_name`, `staff_date_of_joining`, `staff_phone`, `staff_emergency_contact`, `staff_marital_status`, `staff_photo`, `staff_current_address`, `staff_permanent_address`, `staff_qualification`, `staff_work_experience`, `staff_note`, `staff_epf_no`, `staff_basic_salary`, `staff_contract_type`, `staff_work_shift`, `staff_work_location`, `staff_leaves`, `staff_account_details`, `staff_social_media`, `staff_upload_documents`, `staff_barcode`, `staff_notification_email`, `mobile_api_url`, `app_primary_color_code`, `app_secondary_color_code`, `admin_mobile_api_url`, `admin_app_primary_color_code`, `admin_app_secondary_color_code`, `app_logo`, `student_profile_edit`, `start_week`, `my_question`, `superadmin_restriction`, `student_timeline`, `calendar_event_reminder`, `event_reminder`, `student_login`, `parent_login`, `student_panel_login`, `parent_panel_login`, `is_student_feature_lock`, `maintenance_mode`, `lock_grace_period`, `is_offline_fee_payment`, `offline_bank_payment_instruction`, `created_at`, `updated_at`) VALUES
+(1, 'http://localhost/sunil/', 'C:\\xampp\\htdocs\\sunil\\', 'Your School Name', 0, '', 'yourschoolemail@domain.com', 'Your School Phone', 'Your School Address', 4, '[\"4\"]', 'Your School Code', 'm/d/Y', '12-hour', '150', '$', 'disabled', '0', 0, 0, 'UTC', 18, '', 'after_number', '####.##', 'no', '4', 0, 0.00, '1.png', '1.png', '1.png', '1663064530-1070210809632059d2b8b0b!1662796232-1721792380631c41c80d038!login_bg3.jpg', '1663065284-93117584263205cc49769c!1662964519-2099955753631ed327d0ffa!login_bg5.jpg', 'gray.jpg', 60, 0, '', '', 0, 1, 0, '', '', 0, 1, 'no', 0, '', 0, '', '<p>&nbsp;Please enter your institution online admission terms & conditions here.</p>', NULL, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, '', '', '#424242', '#eeeeee', '', '', '', '1.png', 0, 'Monday', 0, 'enabled', 'disabled', 0, 'disabled', 'null', 'null', 1, 1, 0, 0, 0, 0, '', '2023-09-11 18:52:56', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `sch_settings`
+--
+ALTER TABLE `sch_settings`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `lang_id` (`lang_id`),
+  ADD KEY `session_id` (`session_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
